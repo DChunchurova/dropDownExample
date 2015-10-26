caeb3363d1c2cc7b4cc9bb13bc1dc2b0b70e276f
nikolay.torbev <ntorbev@gmail.com>
2015-10-26 22:27:30 +0200

    styles and img backgr

--- dropDown.js
+++ dropDown.js
@@ -1,19 +1,25 @@
 $(function(){ 
 	var countries = {
-		Bulgaria:['sofia','vidin','burgas'],
-		France:['paris', 'lyeon','dijon']
+		Bulgaria:['Sofia','Vidin','Burgas','Plovdiv','Varna','Lovech'],
+		France:['Paris', 'Lyeon','Dijon'],
+		Germany:['Hamburg', 'Berlin','Munich'],
+		Italy:['Rome', 'Milan','Bari','Palermo']
 	}
 
 	function search(evt){
 		var country = this.value,
 			cities = evt.data.countries[country];
-			$("#result").html('')
-		for (var i = 0; i < cities.length; i++) {
+			
+		$("#result").html('')
+		for (var i = 0; i <cities.length; i++) {
 			$("#result").append('<div>'+ cities[i] +'</div>')
 		};
+		$("#result div:odd").addClass("odd");
 
 	}
 	
 	$("#choose").on('change',{countries:countries},search);
+	
 
 })
+$("#result:odd").addClass("odd");
